40ced31aee0093d0a1c506ea412a82870fe7dfec
lorenzo-27 <lorenzo.benedetti5@stud.unifi.it>
2025-01-01 22:56:22 +0100

    Solved major bugs

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import yaml
 from ipdb import launch_ipdb_on_exception
 from rich.logging import RichHandler
 
-from models import m_cnn, m_fcn, m_unet, m_unet_dropout, m_unet_embedding, m_unet_dropout_embedding
+from models import m_cnn, m_fcn, m_unet, m_unet_dropout, m_unet_embedding, m_unet_dropout_embedding, m_o_unet
 
 CNN = m_cnn.CNN
 FCN = m_fcn.FCN
@@ -18,7 +18,7 @@ UNet = m_unet.UNet
 UNetDropout = m_unet_dropout.UNetDropout
 UNetEmbedding = m_unet_embedding.UNetEmbedding
 UNetDropoutEmbedding = m_unet_dropout_embedding.UNetDropoutEmbedding
-
+OUNet = m_o_unet.OUNet
 
 def get_logger():
     FORMAT = "%(message)s"
@@ -28,13 +28,7 @@ def get_logger():
     log = logging.getLogger("rich")
     return log
 
-
 LOG = get_logger()
-
-
-def N(x):
-    """Converte un tensore Torch in un array NumPy."""
-    return x.detach().cpu().numpy()
 
 
 def save_checkpoint(model, optimizer, epoch, loss, opts):
@@ -47,96 +41,84 @@ def save_checkpoint(model, optimizer, epoch, loss, opts):
     }
     torch.save(checkpoint, fname)
     LOG.info(f"Saved checkpoint {fname}")
+    
+def log_metrics(writer, metrics, epoch):
+    with writer.as_default():
+        for key, value in metrics.items():
+            tf.summary.scalar(key, value, step=epoch)
 
-
-def train_loop(model, train_loader, val_loader, test_loader, cb513_test_loader, opts):
+def train_loop(model, train_loader, cb513_test_loader, opts):
     """Funzione principale per il ciclo di training."""
-    train_writer = tf.summary.create_file_writer(
-        f"tensorboard/{opts.model['name']}/train"
-    )
-    val_writer = tf.summary.create_file_writer(f"tensorboard/{opts.model['name']}/val")
-    test_writer = tf.summary.create_file_writer(
-        f"tensorboard/{opts.model['name']}/test"
-    )
-    cb513_writer = tf.summary.create_file_writer(
-        f"tensorboard/{opts.model['name']}/cb513"
-    )
+    train_writer = tf.summary.create_file_writer(f"tensorboard/{opts.model['name']}/train")
+    cb513_writer = tf.summary.create_file_writer(f"tensorboard/{opts.model['name']}/cb513")
 
     optimizer = torch.optim.AdamW(
         model.parameters(),
         lr=opts.training["learning_rate"],
-        weight_decay=opts.training["weight_decay"],
+        weight_decay=opts.training["weight_decay"]
     )
     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, min_lr=1e-6)
     loss_function = torch.nn.CrossEntropyLoss()
 
     step = 0
-    best_val_accuracy = 0
-    best_val_loss = np.inf
+    best_cb513_loss = float("inf")
     best_model_state = None
     epochs_without_improvement = 0
     early_stopping_patience = opts.training["early_stopping_patience"]
 
     for epoch in range(1, opts.training["num_epochs"] + 1):
         model.train()
-        train_losses, train_accuracies = [], []
-        for batch_idx, (X, Y) in enumerate(train_loader):
-            X, Y = X.to(opts.device), Y.to(opts.device)
-            optimizer.zero_grad()
+
+        train_total_loss = 0
+        train_q8_correct = 0
+        train_total_valid_positions = 0
+
+        for batch_idx, (X, y, mask) in enumerate(train_loader):
+            X, y, mask = X.to(opts.device), y.to(opts.device), mask.to(opts.device)
+            optimizer.zero_grad(set_to_none=True)
             outputs = model(X)
-            loss = loss_function(outputs.reshape(-1, 8), Y.reshape(-1, 8).argmax(dim=1))
-            loss = torch.mean(loss)  # serve per evitare un errore di dimensione
+
+            loss = loss_function(outputs, y)
+            loss = (loss * mask).sum() / mask.sum()
             loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
-            # Calcolo metriche
-            train_losses.append(loss.item())
-            predictions = torch.argmax(outputs, dim=1)
-            accuracy = (predictions == Y.argmax(dim=1)).float().mean().item()
-            train_accuracies.append(accuracy)
+            train_total_loss += loss.item()
+            train_q8_predictions = torch.argmax(outputs, dim=1)
+            train_q8_labels = torch.argmax(y, dim=1)
+            train_q8_correct += ((train_q8_predictions == train_q8_labels) * mask).sum().item()
+            train_total_valid_positions += mask.sum().item()
 
             if batch_idx % opts.training["log_every"] == 0:
-                avg_loss = np.mean(train_losses[-opts.training["batch_window"] :])
-                avg_acc = np.mean(train_accuracies[-opts.training["batch_window"] :])
-                LOG.info(
-                    f"Epoch {epoch}, Batch {batch_idx}: Loss={avg_loss:.6f}, Accuracy={avg_acc:.3f}"
-                )
-                log_metrics(train_writer, {"train_loss": avg_loss, "train_accuracy": avg_acc}, step)
+                train_avg_loss = train_total_loss / (batch_idx + 1)
+                train_q8_accuracy = train_q8_correct / train_total_valid_positions
+                log_metrics(train_writer, {"train_loss": train_avg_loss, "train_accuracy": train_q8_accuracy}, step)
+                LOG.info(f"Epoch {epoch}, Batch {batch_idx}: Loss={train_avg_loss:.6f}, [Q8]Acc={train_q8_accuracy:.3f}")
                 step += 1
 
         if opts.training["include_q3"] == False:
-            val_loss, val_q8_accuracy = evaluate(model, val_loader, loss_function, opts)
-            test_loss, test_q8_accuracy = evaluate(model, test_loader, loss_function, opts)
             cb513_loss, cb513_q8_accuracy = evaluate(model, cb513_test_loader, loss_function, opts)
-            log_metrics(val_writer, {"loss": val_loss, "accuracy": val_q8_accuracy}, epoch)
-            log_metrics(test_writer, {"loss": test_loss, "accuracy": test_q8_accuracy}, epoch)
             log_metrics(cb513_writer, {"loss": cb513_loss, "accuracy": cb513_q8_accuracy}, epoch)
-            LOG.info(f"Epoch {epoch}: Acc[Q8] - ValLoss = {val_loss:.6f}, ValAcc={val_q8_accuracy:.3f}, TestLoss={test_loss:.6f}, TestAcc={test_q8_accuracy:.3f}, CB513Loss = {cb513_loss:.6f}, CB513Acc={cb513_q8_accuracy:.3f}")
-
-        elif opts.training["include_q3"] == True:
-            val_loss, val_q8_accuracy, val_q3_accuracy = evaluate(model, val_loader, loss_function, opts)
-            test_loss, test_q8_accuracy, test_q3_accuracy = evaluate(model, test_loader, loss_function, opts)
-            cb513_loss, cb513_q8_accuracy, cb513_q3_accuracy = evaluate(model, cb513_test_loader, loss_function, opts)
-            log_metrics(val_writer, {"loss": val_loss, "accuracy": val_q8_accuracy, "accuracy": val_q3_accuracy}, epoch)
-            log_metrics(test_writer, {"loss": test_loss, "accuracy": test_q8_accuracy, "accuracy": test_q3_accuracy}, epoch)
-            log_metrics(cb513_writer, {"loss": cb513_loss, "accuracy": cb513_q8_accuracy, "accuracy": cb513_q3_accuracy}, epoch)
-            LOG.info(f"Epoch {epoch}: Acc[Q8/Q3] - ValLoss = {val_loss:.6f}, ValAcc={val_q8_accuracy:.3f}/{val_q3_accuracy:.3f}, TestLoss={test_loss:.6f}, TestAcc={test_q8_accuracy:.3f}/{test_q3_accuracy:.3f}, CB513Loss = {cb513_loss:.6f}, CB513Acc={cb513_q8_accuracy:.3f}/{cb513_q3_accuracy:.3f}")
+            LOG.info(f"Epoch {epoch}: CB513Loss={cb513_loss:.6f}, [Q8]CB513Acc={cb513_q8_accuracy:.3f}")
 
         else:
-            raise ValueError(f"Unknown train type {opts.training['include_q3']}")
+            cb513_loss, cb513_q8_accuracy, cb513_q3_accuracy = evaluate(model, cb513_test_loader, loss_function, opts)
+            log_metrics(cb513_writer, {"loss": cb513_loss, "accuracy": cb513_q8_accuracy, "accuracy": cb513_q3_accuracy}, epoch)
+            LOG.info(f"Epoch {epoch}: CB513Loss={cb513_loss:.6f}, [Q8/Q3]CB513Acc={cb513_q8_accuracy:.3f}/{cb513_q3_accuracy:.3f}")
 
         # Early stopping check
-        if val_q8_accuracy >= best_val_accuracy and val_loss <= best_val_loss:
-            best_val_accuracy = val_q8_accuracy
-            best_val_loss = val_loss
+        if cb513_loss <= best_cb513_loss:
+            best_cb513_loss = cb513_loss
             best_model_state = model.state_dict()
             epochs_without_improvement = 0
         else:
             epochs_without_improvement += 1
 
         if epochs_without_improvement >= early_stopping_patience:
-            LOG.info(f"Early stopping triggered after {epoch} epochs. Maximum Validation Q8 accuracy: {best_val_accuracy:.3f}. Minimum Validation loss: {best_val_loss:.6f}")
+            LOG.info(f"Early stopping triggered after {epoch} epochs. Minimum cb513 loss: {best_cb513_loss:.6f}")
             if best_model_state is not None:
                 model.load_state_dict(best_model_state)
                 save_checkpoint(model, optimizer, epoch, loss.item(), opts)
@@ -148,16 +130,18 @@ def train_loop(model, train_loader, val_loader, test_loader, cb513_test_loader, 
 
         # scheduler.step()
         scheduler.step(cb513_loss)
+        LOG.info(f"LR: {scheduler.get_last_lr()}")
 
 def evaluate(model, loader, loss_function, opts):
     """Calcola la loss e l'accuratezza Q8 e Q3 sul dataset di validazione o test."""
     model.eval()
 
     total_loss = 0
-    q8_correct_predictions = 0
-    q8_total_predictions = 0
+    q8_correct = 0
+    total_valid_positions = 0
 
-    if opts.training["include_q3"] == True:
+    if opts.training["include_q3"]:
+        q3_correct_predictions = 0
         # Mappatura da Q8 a Q3
         q8_to_q3 = {
             0: 0,  # H -> H (helix)
@@ -169,49 +153,42 @@ def evaluate(model, loader, loss_function, opts):
             6: 2,  # S -> C (coil)
             7: 2,  # C -> C (coil)
         }
-        q3_correct_predictions = 0
-        q3_total_predictions = 0
 
     with torch.no_grad():
-        for X, Y in loader:
-            X, Y = X.to(opts.device), Y.to(opts.device)
+        for X, Y, mask in loader:
+            X, Y, mask = X.to(opts.device), Y.to(opts.device), mask.to(opts.device)
             outputs = model(X)
 
-            loss = loss_function(outputs.reshape(-1, 8), Y.reshape(-1, 8).argmax(dim=1))
+            loss = loss_function(outputs, Y)
+            loss = (loss * mask).sum() / mask.sum()
             total_loss += loss.item()
 
             q8_predictions = torch.argmax(outputs, dim=1)
-            q8_labels = Y.argmax(dim=1)
-            q8_correct_predictions += (q8_predictions == q8_labels).sum().item()
-            q8_total_predictions += q8_labels.numel()
+            q8_labels = torch.argmax(Y, dim=1)
+            q8_correct += ((q8_predictions == q8_labels) * mask).sum().item()
+            total_valid_positions += mask.sum().item()
 
-            if opts.training["include_q3"] == True:
+            if opts.training["include_q3"]:
                 q3_predictions = torch.tensor(
                     [q8_to_q3[p.item()] for p in q8_predictions.flatten()],
                     device=opts.device,
                 ).reshape(q8_predictions.shape)
+
                 q3_labels = torch.tensor(
                     [q8_to_q3[l.item()] for l in q8_labels.flatten()],
                     device=opts.device,
                 ).reshape(q8_labels.shape)
-                q3_correct_predictions = (q3_predictions == q3_labels).sum().item()
-                q3_total_predictions = q3_labels.numel()
+
+                q3_correct_predictions = ((q3_predictions == q3_labels) * mask).sum().item()
 
     avg_loss = total_loss / len(loader)
-    q8_accuracy = q8_correct_predictions / q8_total_predictions
+    q8_accuracy = q8_correct / total_valid_positions
 
-    if opts.training["include_q3"] == False:
-        return avg_loss, q8_accuracy
-    elif opts.training["include_q3"] == True:
-        q3_accuracy = q3_correct_predictions / q3_total_predictions
+    if opts.training["include_q3"]:
+        q3_accuracy = q3_correct_predictions / total_valid_positions
         return avg_loss, q8_accuracy, q3_accuracy
     else:
-        raise ValueError(f"Unknown train type {opts.training['include_q3']}")
-
-def log_metrics(writer, metrics, epoch):
-    with writer.as_default():
-        for key, value in metrics.items():
-            tf.summary.scalar(key, value, step=epoch)
+        return avg_loss, q8_accuracy
 
 def main(opts):
     from dataset import load_data
@@ -282,6 +259,20 @@ def main(opts):
             "unet_dropout_embedding",
             torch.randn(opts.training["batch_size"], opts.model["in_channels"], 700),
         )
+    elif opts.model["name"] == "o_unet":
+        model = OUNet(
+            in_channels=opts.model["in_channels"],
+            out_channels=opts.model["out_channels"],
+            base_filters=opts.model["base_filters"],
+            kernel_size=opts.model["kernel_size"],
+            dropout_rate=opts.model["dropout_rate"],
+            depth=opts.model["depth"],
+        )
+        visualize(
+            model,
+            "o_unet",
+            torch.randn(opts.training["batch_size"], opts.model["in_channels"], 700),
+        )
     else:
         raise ValueError(f"Modello sconosciuto {opts.model['name']}")
 
@@ -289,37 +280,10 @@ def main(opts):
     LOG.info("Caricamento del dataset")
 
     # Caricamento dei dati
-    train_X, train_y, val_X, val_y, test_X, test_y = load_data(
-        opts.dataset["train_path"], opts.dataset["seed"]
-    )
-    cb513_test_X, cb513_test_y = load_data(
-        opts.dataset["test_path"], opts.dataset["seed"]
-    )
-
-    # Creazione dei DataLoader
-    train_loader = torch.utils.data.DataLoader(
-        dataset=list(zip(torch.tensor(train_X), torch.tensor(train_y))),
-        batch_size=opts.training["batch_size"],
-        shuffle=True,
-    )
-    val_loader = torch.utils.data.DataLoader(
-        dataset=list(zip(torch.tensor(val_X), torch.tensor(val_y))),
-        batch_size=opts.training["batch_size"],
-        shuffle=False,
-    )
-    test_loader = torch.utils.data.DataLoader(
-        dataset=list(zip(torch.tensor(test_X), torch.tensor(test_y))),
-        batch_size=opts.training["batch_size"],
-        shuffle=False,
-    )
-    cb513_test_loader = torch.utils.data.DataLoader(
-        dataset=list(zip(torch.tensor(cb513_test_X), torch.tensor(cb513_test_y))),
-        batch_size=opts.training["batch_size"],
-        shuffle=False,
-    )
+    train_loader, cb513_test_loader = load_data(opts.training["batch_size"])
 
     LOG.info("Inizio del training")
-    train_loop(model, train_loader, val_loader, test_loader, cb513_test_loader, opts)
+    train_loop(model, train_loader, cb513_test_loader, opts)
 
 
 if __name__ == "__main__":
